948aaa0922f66586e0578893b1feb4bdae2063be
victoriapl01 <victoria.pradaslaguna@student.hogent.be>
2023-02-18 17:37:38 +0100

    Detalles concretos e inclusión alert

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ const form = document.getElementById('formulario');
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que se envíe el formulario automáticamente
 
+    //Variables del formulario
     const nombre = document.getElementById('nombre');
     const email = document.getElementById('email');
     const clave = document.getElementById('clave');
     const conf_clave = document.getElementById('conf_clave');
 
+    //Variable local que identifica si los valores son válidos
     let campos_validos = true;
 
     // Validación del campo nombre
     if (nombre.value === '') {
         campos_validos = false;
+        //Eliminación del campo válido por si anteriormente se ha escrito de forma válida
+        nombre.classList.remove('campo-valido');
+        nombre.classList.remove('successIcon');
         nombre.classList.add('campo-invalido');
         nombre.classList.add('errorIcon');
         nombre.nextElementSibling.textContent = 'Rellene este campo';
     } else if (!/^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s.,;:"'()¿?!_¡-]+$/g.test(nombre.value)) {
         campos_validos = false;
+        nombre.classList.remove('campo-valido');
+        nombre.classList.remove('successIcon');
         nombre.classList.add('campo-invalido');
         nombre.classList.add('errorIcon');
         nombre.nextElementSibling.textContent = 'No se permiten caractéres numéricos';
@@ -32,11 +39,15 @@ form.addEventListener('submit', function(event) {
     // Validación del campo email
     if (email.value === '') {
         campos_validos = false;
+        email.classList.remove('campo-valido');
+        email.classList.remove('successIcon');
         email.classList.add('campo-invalido');
         email.classList.add('errorIcon');
         email.nextElementSibling.textContent = 'Rellene este campo';
     } else if (!/^\S+@\S+\.\S+$/.test(email.value)) {
         campos_validos = false;
+        email.classList.remove('campo-valido');
+        email.classList.remove('successIcon');
         email.classList.add('campo-invalido');
         email.classList.add('errorIcon');
         email.nextElementSibling.textContent = 'Email inválido';
@@ -51,11 +62,15 @@ form.addEventListener('submit', function(event) {
     // Validación del campo clave
     if (clave.value === '') {
         campos_validos = false;
+        clave.classList.remove('campo-valido');
+        clave.classList.remove('successIcon');
         clave.classList.add('campo-invalido');
         clave.classList.add('errorIcon');
         clave.nextElementSibling.textContent = 'Rellene este campo';
     } else if (!/^[ \s\S]{1,8}$/.test(clave.value)) {
         campos_validos = false;
+        clave.classList.remove('campo-valido');
+        clave.classList.remove('successIcon');
         clave.classList.add('campo-invalido');
         clave.classList.add('errorIcon');
         clave.nextElementSibling.textContent = 'No debe tener más de 8 caracteres';
@@ -70,6 +85,8 @@ form.addEventListener('submit', function(event) {
     // Validación del campo conf_clave
     if (conf_clave.value === '') {
         campos_validos = false;
+        conf_clave.classList.remove('campo-valido');
+        conf_clave.classList.remove('successIcon');
         conf_clave.classList.add('campo-invalido');
         conf_clave.classList.add('errorIcon');
         conf_clave.nextElementSibling.textContent = 'Rellene este campo';
@@ -81,11 +98,14 @@ form.addEventListener('submit', function(event) {
         conf_clave.nextElementSibling.textContent = '';
     } else {
         campos_validos = false;
+        conf_clave.classList.remove('campo-valido');
+        conf_clave.classList.remove('successIcon');
         conf_clave.classList.add('campo-invalido');
         conf_clave.classList.add('errorIcon');
         conf_clave.nextElementSibling.textContent = 'La contraseñas no coinciden';
     }
 
+    //Alert indicando que la inscripción ha sido correcta
     if (campos_validos) {
         alert('Inscripción completada correctamente');
     }
