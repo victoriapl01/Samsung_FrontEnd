07b0720eed6d38e65b8a2b8268a5d27a149dd7df
victoriapl01 <victoria.pradaslaguna@student.hogent.be>
2023-02-18 16:55:37 +0100

    Validación completa

--- index.js
+++ index.js
@@ -11,43 +11,52 @@ form.addEventListener('submit', function(event) {
     // Validación del campo nombre
     if (nombre.value === '') {
         nombre.classList.add('campo-invalido');
-        nombre.nextElementSibling.innerText = 'Rellene este campo';
-    } else if (!/^[a-zA-Z]+$/.test(nombre.value)) {
+        nombre.nextElementSibling.textContent = 'Rellene este campo';
+    } else if (!/^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s.,;:"'()¿?!_¡-]+$/g.test(nombre.value)) {
         nombre.classList.add('campo-invalido');
-        nombre.nextElementSibling.innerText = 'No se permiten numeros';
+        nombre.nextElementSibling.textContent = 'No se permiten caractéres numéricos';
     } else {
         nombre.classList.remove('campo-invalido');
         nombre.classList.add('campo-valido');
-        nombre.nextElementSibling.innerText = '';
+        nombre.nextElementSibling.textContent = '';
     }
 
     // Validación del campo email
-    if (email.value === '' || !/^\S+@\S+\.\S+$/.test(email.value)) {
+    if (email.value === '') {
         email.classList.add('campo-invalido');
-        email.nextElementSibling.innerText = 'Rellene este campo';
+        email.nextElementSibling.textContent = 'Rellene este campo';
+    } else if (!/^\S+@\S+\.\S+$/.test(email.value)) {
+        email.classList.add('campo-invalido');
+        email.nextElementSibling.textContent = 'Email inválido';
     } else {
-        email.classList.remove('campo-invalido')
+        email.classList.remove('campo-invalido');
         email.classList.add('campo-valido');
-        email.nextElementSibling.innerText = '';
+        email.nextElementSibling.textContent = '';
     }
 
     // Validación del campo clave
     if (clave.value === '') {
         clave.classList.add('campo-invalido');
-        clave.nextElementSibling.innerText = 'Rellene este campo';
+        clave.nextElementSibling.textContent = 'Rellene este campo';
+    } else if (!/^[ \s\S]{1,8}$/.test(clave.value)) {
+        clave.classList.add('campo-invalido');
+        clave.nextElementSibling.textContent = 'No debe tener más de 8 caracteres';
     } else {
-        email.classList.remove('campo-invalido')
-        email.classList.add('campo-valido');
-        clave.nextElementSibling.innerText = '';
+        clave.classList.remove('campo-invalido');
+        clave.classList.add('campo-valido');
+        clave.nextElementSibling.textContent = '';
     }
 
     // Validación del campo conf_clave
-    if (conf_clave.value === '' || conf_clave.value !== clave.value) {
+    if (conf_clave.value === '') {
         conf_clave.classList.add('campo-invalido');
-        conf_clave.nextElementSibling.innerText = 'Rellene este campo';
+        conf_clave.nextElementSibling.textContent = 'Rellene este campo';
+    } else if (conf_clave.value === clave.value) {
+        conf_clave.classList.remove('campo-invalido');
+        conf_clave.classList.add('campo-valido');
+        conf_clave.nextElementSibling.textContent = '';
     } else {
-        email.classList.remove('campo-invalido')
-        email.classList.add('campo-valido');
-        conf_clave.nextElementSibling.innerText = '';
+        conf_clave.classList.add('campo-invalido');
+        conf_clave.nextElementSibling.textContent = 'La contraseñas no coinciden';
     }
 });
